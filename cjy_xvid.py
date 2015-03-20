f7105cf5ef9a32f6c218a7f3788241b9e533077b
cjy <chen.wahaha@gmail.com>
2015-03-20 16:36:20 +0800

    Add paras in command line

--- cjy_xvid.py
+++ cjy_xvid.py
@@ -30,19 +30,35 @@ def encode_noxvid(src, save_folder, dst, qs, is_yuvout, yuv_folder):
 
 result_folder = "/home/cjy/Desktop/MBStatistic" + "/";
 yuv_folder = "/home/cjy/Desktop/YUV" + "/";
-if(len(sys.argv) > 1):
-    video = sys.argv[1];
-else:
-    video = "akiyo";
-print video;
+video = "akiyo";
 qs = "5";
+num = 11;
+if(len(sys.argv) > 1):
+    i = 1;
+    while(i < len(sys.argv)):
+        if(sys.argv[i] == "-v"):
+            video = sys.argv[i + 1];
+            i += 2;
+        elif(sys.argv[i] == "-q"):
+            qs = sys.argv[i + 1];
+            i += 2;
+        elif(sys.argv[i] == "-r"):
+            result_folder = sys.argv[i + 1];
+            i += 2;
+        elif(sys.argv[i] == "-n"):
+            num = int(sys.argv[i + 1]);
+            i += 2;
+        else:
+            print "Wrong para";
+            exit(-1);
+
 src = yuv_folder + video + "_cif.yuv";
 origin = 1;
 yuv_save_folder = result_folder + video + "_q" + qs + "/" + video + "_yuv";
 #checkpath
 if not os.path.exists(yuv_save_folder):
     os.makedirs(yuv_save_folder);
-for i in range (1,11):
+for i in range (1,num):
     folder = result_folder + video + "_q" + qs;
     save_folder = folder + "/" + video + "_q" + qs + "_" + str(i) + "_enc";
     dst = folder + "/" + video + "_q" + qs + "_" + str(i) + ".m4v";
